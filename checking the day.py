5fb0b92fae3ecdecd71f0ebbee5b2bb3fd7d64b7
Njeriee <87851470+Njeriee@users.noreply.github.com>
2022-01-20 16:46:08 +0300

    Add files via upload

---
+++ checking the day.py
@@ -0,0 +1,8 @@
+import datetime
+datetime.date.today().day
+datetime.date.today().month
+datetime.date.today().year
+datetime.date.isoformat(datetime.date.today())
+import time
+time.strftime("%H:%M")
+time.strftime("%A %p")
